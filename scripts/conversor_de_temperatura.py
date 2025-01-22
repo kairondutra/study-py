cbac03ed651cb8935a66e1b293142b66e1290e49
Kairon <34685175+kaironlucas@users.noreply.github.com>
2025-01-21 23:12:19 -0300

    melhorias no conversor de temperaturas

--- scripts/conversor_de_temperatura.py
+++ scripts/conversor_de_temperatura.py
@@ -4,7 +4,7 @@ def conversor_de_temperatura():
     
     while True:
         # 1. Menu de opções
-        print("=== Conversor de Temperaturas ===")
+        print("=== Conversor de Temperaturas ===".center(40, "="))
         print("1. Celsius para Fahrenheit")
         print("2. Fahrenheit para Celsius")
         print("3. Celsius para Kelvin")
@@ -32,26 +32,22 @@ def conversor_de_temperatura():
         
         # Verifica se a operação escolhida é válida (1-7)
         if opcao not in [1, 2, 3, 4, 5, 6, 7]:
-            print("Operação inválida. Escolha uma das opções (1-7).")
+            print("Escolha uma opção válida entre 1 e 7!")
             input("Pressione Enter para continuar...")
             os.system('cls' if os.name == 'nt' else 'clear')
             continue
         
         # Solicita os dois valores para a operação
         try:
-            if opcao == 1:
-                user = float(input("Insira o valor em Celsius: "))
-            elif opcao == 2:
-                user = float(input("Insira o valor em Fahrenheit: "))
-            elif opcao == 3:
-                user = float(input("Insira o valor em Celsius: "))
-            elif opcao == 4:
-                user = float(input("Insira o valor em Kelvin: "))
-            elif opcao == 5:
-                user = float(input("Insira o valor em Fahrenheit: "))
-            elif opcao == 6:
-                user = float(input("Insira o valor em Kelvin: "))
-            
+            mensagens = {
+                1: "Insira o valor em Celsius: ",
+                2: "Insira o valor em Fahrenheit: ",
+                3: "Insira o valor em Celsius: ",
+                4: "Insira o valor em Kelvin: ",
+                5: "Insira o valor em Fahrenheit: ",
+                6: "Insira o valor em Kelvin: ",
+            }
+            user = float(input(mensagens[opcao]))
 
         except ValueError:
             # Se o usuário inserir um valor inválido, exibe uma mensagem de erro e reinicia o loop
@@ -59,32 +55,37 @@ def conversor_de_temperatura():
             input("Pressione Enter para continuar...")
             os.system('cls' if os.name == 'nt' else 'clear')
             continue
+        
+        if opcao in [3, 4, 6] and user < 0:
+            print("O valor em Kelvin não pode ser negativo!")
+            input("Pressione Enter para continuar...")
+            continue
             
         # Executa a operação escolhida
         if opcao == 1:
             # Celsius para Fahrenheit
             resultado = float((user * 9/5) + 32 )
-            print(f"{user}°C = {resultado:.2f}°F\n")
+            print(f"Conversão concluida!\n{user}°C convertido em Fahrenheit é {resultado:.2f}°F\n")
         elif opcao == 2:
             # Fahrenheit para Celsius
-            resultado = float((user - 32) / 1.8)
-            print(f"{user}°F = {resultado:.2f}°C\n")
+            resultado = (user - 32) / 1.8
+            print(f"Conversão concluida!\n{user}°F convertido em Celsius é {resultado:.2f}°C\n")
         elif opcao == 3:
             # Celsius para Kelvin
-            resultado = float(user + 273.15)
-            print(f"{user}°C = {resultado:.2f}°K\n")
+            resultado = user + 273.15
+            print(f"Conversão concluida!\n{user}°C convertido em Kelvin é {resultado:.2f}°K\n")
         elif opcao == 4:
-            # Kelvin para Celcius
-            resultado = float(user - 273.15)
-            print(f"{user}°K = {resultado:.2f}°C\n")
+            # Kelvin para Celsius
+            resultado = user - 273.15
+            print(f"Conversão concluida!\n{user}°K convertido em Celsius é {resultado:.2f}°C\n")
         elif opcao == 5: 
             # Fahrenheit para Kelvin 
-            resultado = float(((user + 459.67) * 5)/9)
-            print(f"{user}°F = {resultado:.2f}°K\n")
+            resultado = ((user + 459.67) * 5) / 9
+            print(f"Conversão concluida!\n{user}°F convertido em Kelvin é {resultado:.2f}°K\n")
         elif opcao == 6:
             # Kelvin para Fahrenheit
-            resultado = float((user * 1.8) - 459.67)
-            print(f"{user}°K = {resultado:.2f}°F\n")
+            resultado = (user * 1.8) - 459.67
+            print(f"Conversão concluida!\n{user}°K convertido em Fahrenheit é {resultado:.2f}°F\n")
         
         # Pausa para o usuário ver o resultado antes de limpar a tela
         input("Pressione Enter para continuar...")
